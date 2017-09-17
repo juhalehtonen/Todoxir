adc241797f68e9568816fae779de77c89333daad
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2017-09-17 12:50:00 +0300

    Add buttons to edit name

--- assets/js/components/TodoItem.vue
+++ assets/js/components/TodoItem.vue
@@ -1,7 +1,33 @@
 <template>
-  <li class="c-todo-items__item" v-bind:class="{ complete: complete }">
-    <p>{{name}}</p>
-  </li>
+  <div>
+    <input
+         v-if="is_editing"
+         v-model.trim="name"
+         @keyup.enter="submit"
+         type="text"
+         class="c-todo-items__item"
+         v-bind:class="{ complete: complete }"
+         placeholder="Todo"
+    />
+    <li  v-else class="c-todo-items__item" v-bind:class="{ complete: complete }">
+      <p>{{name}}</p>
+    </li>
+
+    <!-- Edit buttons -->
+    <nav v-if="is_editing">
+      <!-- Save button -->
+      <button v-on:click="submit">Save</button>
+      <!-- Cancel button -->
+      <button v-on:click="toggleEdit">Cancel</button>
+    </nav>
+    <div v-else>
+      <!-- Enter to edit mode -->
+      <button v-on:click="toggleEdit">Edit</button>
+    </div>
+
+    <!-- Errors -->
+    <aside v-show="error">{{error}}</aside>
+  </div>
 </template>
 
 <script>
@@ -11,7 +37,8 @@
         name: this.todo.name,
         id: this.todo.id,
         complete: this.todo.complete,
-        is_editing: false
+        is_editing: false,
+        error: null
       }
     },
 
@@ -22,6 +49,34 @@
     },
 
     methods: {
+      // Toggle Edit mode of a todo item
+      toggleEdit () {
+        this.error = null
+        this.is_editing = !this.is_editing
+      },
+
+      submit () {
+        // Update link
+        this.$http.patch(`todos/${this.todo.id}`, {
+          todo: { name: this.name }
+        }).then((response) => {
+          // Success
+          this.error = null
+          this.toggleEdit()
+        }, (response) => {
+          // Error
+          return response.json()
+        }).then((data) => {
+          // Show error message
+          if (data) {
+            this.error = data.errors[0]
+            this.path = this.tempPath
+          } else {
+            this.tempPath = this.path
+          }
+        })
+      }
+
     }
   }
 </script>
